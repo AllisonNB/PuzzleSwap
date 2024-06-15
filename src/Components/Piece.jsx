8b6dbefff57945426ec4760db586f75d908852c5
Allison <allison.nancy.brown34@gmail.com>
2024-06-15 17:38:50 -0400

    made images draggable

--- src/Components/Piece.jsx
+++ src/Components/Piece.jsx
@@ -1,4 +1,8 @@
-import React from 'react';
+import { useRef, useEffect, useState } from 'react';
+import { draggable } from '@atlaskit/pragmatic-drag-and-drop/element/adapter';
+import invariant from 'tiny-invariant';
+
+
 import styled from 'styled-components';
 
 
@@ -7,12 +11,33 @@ const Item = styled.img`
     max-width: 100%;
     height: auto;
     objectFit: cover;
+    ${props => props.$dragging && 'opacity: 0.2;'}
 `
 
 
 const Piece = ({ image }) => {
+
+    const ref = useRef(null);
+    const [dragging, setDragging] = useState(false);
+
+
+
+    useEffect(() => {
+        const el = ref.current;
+        invariant(el); //throws error if falsy value
+
+        return draggable({
+            element: el,
+            getInitialData: () => ({ src: image }, console.log({ src: image })),
+            onDragStart: () => setDragging(true),
+            onDrop: () => setDragging(false),
+        });
+
+    }, [])
+
+
     return (
-        <Item src={image}></Item>
+        <Item ref={ref} src={image} $dragging={dragging}></Item>
     )
 }
 
